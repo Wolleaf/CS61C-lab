42e3d6c55d7acaee958a905038fcf6a35f7ad3f3
Wolleaf <wolleaflee@gmail.com>
2023-10-21 09:40:29 +0800

    feat:lab01

--- lab01/exercise4/ll_cycle.c
+++ lab01/exercise4/ll_cycle.c
@@ -3,4 +3,21 @@
 
 int ll_has_cycle(node *head) {
     /* TODO: Implement ll_has_cycle */
+    if (head == NULL) {
+        return 0;
+    }
+    node *fast_ptr = head->next;
+    node *slow_ptr = head;
+    while (fast_ptr != NULL)
+    {
+        if (fast_ptr == slow_ptr) {
+            return 1;
+        }
+        if (fast_ptr->next == NULL) {
+            break;
+        }
+        fast_ptr = fast_ptr->next->next;
+        slow_ptr = slow_ptr->next;
+    }
+    return 0;
 }
